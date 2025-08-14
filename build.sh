46935ebc17996b99bd8c61bb5ade16943cbb1450
Estanislao Crivos <estanislaocrivos@gmail.com>
2025-08-14 19:39:03 -0300

    Updates.

--- build.sh
+++ build.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
 set -e
+
+if [ ! -d output ]; then
+  mkdir output
+fi
+
 pdflatex -output-directory=output main.tex
 bibtex output/main || true  # only for bib
 pdflatex -output-directory=output main.tex
